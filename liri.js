c94905fa6a4174224dc534755d98bccb02c3bc10
DallinRodgers <rodgers.dallin@gmail.com>
2019-12-30 11:45:00 -0700

    Update liri.js
    Started OMDb api calls.
    If movie is provided results will be displayed on the console.

--- liri.js
+++ liri.js
@@ -1,11 +1,17 @@
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
+const axios = require("axios");
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
 
+var omdb = keys.omdv;
+
 const whatToDo = process.argv[2];
 
+///////////////////////////
+// Spotify API
+///////////////////////////
 if (whatToDo === "spotify-this-song") {
   if (process.argv[3] !== undefined) {
     let songToSpotify = process.argv[3];
@@ -46,6 +52,40 @@ if (whatToDo === "spotify-this-song") {
       })
       .catch(function(err) {
         console.log(err);
+      });
+  }
+}
+
+if (whatToDo === "movie-this") {
+  // Make a request for a user with a given ID
+  if (process.argv[3] !== undefined) {
+    const movie = process.argv[3];
+    axios
+      .get(`http://www.omdbapi.com/?apikey=f97cbe0b&t=${movie}`)
+      .then(function(response) {
+        // handle success
+        const movieResults = response.data;
+        const title = movieResults.Title;
+        const year = movieResults.Year;
+        const imdbRating = movieResults.Ratings[0].Value;
+        const rottenTomatoesRating = movieResults.Ratings[1].Value;
+        const country = movieResults.Country;
+        const language = movieResults.Language;
+        const plot = movieResults.Plot;
+        const actors = movieResults.Actors;
+
+        console.log(title);
+        console.log(year);
+        console.log(imdbRating);
+        console.log(rottenTomatoesRating);
+        console.log(country);
+        console.log(language);
+        console.log(plot);
+        console.log(actors);
+      })
+      .catch(function(error) {
+        // handle error
+        console.log(error);
       });
   }
 }
