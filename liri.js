efd8c8fd3b25375181f3dd845143ccd3c0ed2493
DallinRodgers <rodgers.dallin@gmail.com>
2019-12-30 16:08:02 -0700

    Update liri.js
    Created do-what-it-says,
    Created spotifyThisSong and movieThisMovie functions

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 var Spotify = require("node-spotify-api");
 const axios = require("axios");
+var fs = require("fs");
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
@@ -8,14 +9,19 @@ var spotify = new Spotify(keys.spotify);
 var omdb = keys.omdv;
 
 const whatToDo = process.argv[2];
+if (whatToDo === "spotify-this-song") {
+  let spotifySong = process.argv[3];
+  spotifyThisSong(spotifySong);
+} else if (whatToDo === "movie-this") {
+  let thisMovie = process.argv[3];
+  movieThisMovie(thisMovie);
+}
 
 ///////////////////////////
 // Spotify API
 ///////////////////////////
-if (whatToDo === "spotify-this-song") {
-  if (process.argv[3] !== undefined) {
-    let songToSpotify = process.argv[3];
-
+function spotifyThisSong(songToSpotify) {
+  if (songToSpotify !== undefined) {
     spotify
       .search({ type: "track", query: songToSpotify })
       .then(function(response) {
@@ -49,13 +55,14 @@ if (whatToDo === "spotify-this-song") {
       });
   }
 }
-
-if (whatToDo === "movie-this") {
+///////////////////////////
+// OMDb API
+///////////////////////////
+function movieThisMovie(thisMovie) {
   // Make a request for a user with a given ID
-  if (process.argv[3] !== undefined) {
-    const movie = process.argv[3];
+  if (thisMovie !== undefined) {
     axios
-      .get(`http://www.omdbapi.com/?apikey=f97cbe0b&t=${movie}`)
+      .get(`http://www.omdbapi.com/?apikey=f97cbe0b&t=${thisMovie}`)
       .then(function(response) {
         // handle success
         const movieResults = response.data;
@@ -115,6 +122,20 @@ if (whatToDo === "movie-this") {
         console.log(error);
       });
   }
+}
+///////////////////////////
+// Do Whatever
+///////////////////////////
+if (whatToDo === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function(error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    var dataArr = data.split(",");
+    if (dataArr[0] === "spotify-this-song") {
+      spotifyThisSong(dataArr[1]);
+    }
+  });
 }
 
 function printSpotify(artists, songName, trackLink, album) {
